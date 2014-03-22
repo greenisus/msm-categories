137780bb373572922e6b6be3440390b24ecca96d
Mike Mayo <greenisus@gmail.com>
2014-03-22 08:07:19 -0700

    remove stuff

--- NSString+MSMCategories.h
+++ NSString+MSMCategories.h
@@ -14,11 +14,5 @@
 
 + (NSString *)pluralizedStringForNoun:(NSString *)noun count:(NSInteger)count;
 + (NSString *)pluralizedStringWithoutCountForNoun:(NSString *)noun count:(NSInteger)count;
-+ (NSString *)csvStringFromArray:(NSArray *)stringArray;
-
-// adapted from http://iphonedevelopertips.com/cocoa/truncate-an-nsstring-and-append-an-ellipsis-respecting-the-font-size.html
-- (NSString*)stringByTruncatingToWidth:(CGFloat)width withFont:(UIFont *)font;
-
-- (BOOL)containsEmoji;
 
 @end
